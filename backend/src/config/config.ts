fe8c06aa61a19d2e33584ddde58304f561b607dd
hpar461 <68981068+hpar461@users.noreply.github.com>
2022-04-28 00:37:29 +1200

    Change port to correct process port (#43)
    process.env.SERVER_PORT -> process.env.PORT

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,20 +1,20 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@fearlessfrogs.afbe2.mongodb.net/myFirstDatabase`;
 
-const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 8080;
+const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 8080;
 
 export const config = {
-    mongo: {
-        username: MONGO_USERNAME,
-        password: MONGO_PASSWORD,
-        url: MONGO_URL
-    },
-    server: {
-        port: SERVER_PORT
-    }
+  mongo: {
+    username: MONGO_USERNAME,
+    password: MONGO_PASSWORD,
+    url: MONGO_URL,
+  },
+  server: {
+    port: SERVER_PORT,
+  },
 };
