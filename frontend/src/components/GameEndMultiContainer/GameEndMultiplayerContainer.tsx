d5690e86f33821c336a8f2ac47b8cfe81cac6324
dlee432 <69739721+dlee432@users.noreply.github.com>
2022-05-20 11:46:55 +1200

    Test game end multiplayer container component (#130)
    * initial tests for multi end container component
    
    * tests for game end multi container component

--- frontend/src/components/GameEndMultiContainer/GameEndMultiplayerContainer.tsx
+++ frontend/src/components/GameEndMultiContainer/GameEndMultiplayerContainer.tsx
@@ -10,6 +10,12 @@ interface Props {
   codeBlock: CodeBlock;
 }
 
+/**
+ * This component is to display the end game statistics and results for the multiplayer game mode. 
+ * It includes the players and their game statistics, such as the CPM, Errors and Accuracy. 
+ * It also includes the isViewCode and codeBlock props in order to render an alternate container 
+ * called the ViewCodeContainer. 
+ */
 function GameEndMultiplayerContainer({
   players,
   isViewCode,
