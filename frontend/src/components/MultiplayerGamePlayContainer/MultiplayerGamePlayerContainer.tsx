81e2c626f4c85b3270dab50c34150432ae68c91e
Dave Shin <100652749+hiin3d55@users.noreply.github.com>
2022-05-20 14:00:29 +1200

    Make tests for MultiplayerGamePlayContainer (#137)
    * Removed unused imports.
    
    * Made tests for WelcomeCode component.
    
    * Added tests for PlayerCardStats component.
    
    * Added comments for the component that is being tested
    
    * Added a test for MultiplayerGamePlayContainer component.

--- frontend/src/components/MultiplayerGamePlayContainer/MultiplayerGamePlayerContainer.tsx
+++ frontend/src/components/MultiplayerGamePlayContainer/MultiplayerGamePlayerContainer.tsx
@@ -15,6 +15,17 @@ interface Props {
   updateStats: (stats: PlayerStats) => void;
 }
 
+/**
+ * This component is used to display the multiplayer game screen. It wraps list of the players and GameContainer.
+ *
+ * @param started - boolean if true lets the user type otherwise does not register their events
+ * @param onGameOver - function that is called when all the text has been typed
+ * @param gameSettings - component that holds language, time, and playerAmount
+ * @param code - code that will be used for the game
+ * @param otherPlayers - array that stores other players involved in the game
+ * @param updateStats - function that updates the players stats based on their performance in the game
+ * @param language - programming language of the code that will be used for the game
+ */
 function MultiplayerGamePlayerContainer({ started, onGameOver, gameSettings, code, otherPlayers, updateStats, language = "javascript" }: Props) {
 
   return (
