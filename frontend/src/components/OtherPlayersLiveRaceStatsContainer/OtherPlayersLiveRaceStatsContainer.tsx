2d98abd1f0dcafdebe29af72ea27d7de3800b5b3
hpar461 <68981068+hpar461@users.noreply.github.com>
2022-05-20 18:33:38 +1200

    Add tests for OtherPlayersLiveRaceStatsContainer & OtherPlayersLiveRaceStatsItem components. (#149)
    * Add tests and comments for item component
    
    * Add tests and comments for container component

--- frontend/src/components/OtherPlayersLiveRaceStatsContainer/OtherPlayersLiveRaceStatsContainer.tsx
+++ frontend/src/components/OtherPlayersLiveRaceStatsContainer/OtherPlayersLiveRaceStatsContainer.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import classes from './OtherPlayersLiveRaceStats.module.css';
 import OtherPlayersLiveRaceStatsItem from "./OtherPlayersLiveRaceStatsItem";
-import {Grid} from "@mui/material";
-import {Player} from "../../utils/Types/SocketTypes";
+import { Grid } from "@mui/material";
+import { Player } from "../../utils/Types/SocketTypes";
 
 interface Props {
-  otherPlayers: Player[]
+  otherPlayers: Player[];
 }
 
-function OtherPlayersLiveRaceStatsContainer({otherPlayers}: Props) {
+/**
+ * Component for displaying the stats of other players in multiplayer game.
+ *
+ * @param otherPlayers The other players in the game
+ * @returns The component to display the live race stats of other players.
+ */
+function OtherPlayersLiveRaceStatsContainer({ otherPlayers }: Props) {
   return (
-    <Grid container rowSpacing={3} columnSpacing={{xs: 2, sm: 3, md: 4}} direction="row" justifyContent="center"
-          alignItems="center">
-      {
-        otherPlayers.map((otherPlayer, index) =>
-          <Grid xs={3} item key={index}>
-            <OtherPlayersLiveRaceStatsItem playerName={otherPlayer.playerName} playerStats={otherPlayer.playerStats}/>
-          </Grid>
-        )
-      }
+    <Grid
+      container
+      rowSpacing={3}
+      columnSpacing={{ xs: 2, sm: 3, md: 4 }}
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+    >
+      {otherPlayers.map((otherPlayer, index) => (
+        <Grid xs={3} item key={index}>
+          <OtherPlayersLiveRaceStatsItem
+            playerName={otherPlayer.playerName}
+            playerStats={otherPlayer.playerStats}
+          />
+        </Grid>
+      ))}
     </Grid>
   );
 }
--- frontend/src/components/OtherPlayersLiveRaceStatsContainer/OtherPlayersLiveRaceStatsItem.tsx
+++ frontend/src/components/OtherPlayersLiveRaceStatsContainer/OtherPlayersLiveRaceStatsItem.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import classes from './OtherPlayersLiveRaceStats.module.css';
-import {Typography} from "@mui/material";
-import {styled} from "@mui/material/styles";
+import classes from "./OtherPlayersLiveRaceStats.module.css";
+import { Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import ProgressBar from "../ProgressBar";
-import {PlayerStats} from "../../utils/Types/SocketTypes";
+import { PlayerStats } from "../../utils/Types/SocketTypes";
 
-const PlayerTypography = styled(Typography)(({theme}) => ({
-  fontWeight: 'bold',
+const PlayerTypography = styled(Typography)(({ theme }) => ({
+  fontWeight: "bold",
   fontSize: 18,
 }));
 
@@ -15,11 +14,20 @@ interface Props {
   playerStats: PlayerStats;
 }
 
-function OtherPlayersLiveRaceStatsItem({playerName, playerStats}: Props) {
+/**
+ * Component for displaying the stats of other players in multiplayer game.
+ * @param playerName The name of the player
+ * @param playerStats The stats of the player
+ * @returns The component to display
+ */
+function OtherPlayersLiveRaceStatsItem({ playerName, playerStats }: Props) {
   return (
     <div className={classes.StatsItemContainer}>
       <PlayerTypography>{playerName}</PlayerTypography>
-      <ProgressBar style={{  marginTop: 5, marginBottom: 5}} progress={playerStats.Progress}/>
+      <ProgressBar
+        style={{ marginTop: 5, marginBottom: 5 }}
+        progress={playerStats.Progress}
+      />
       <PlayerTypography>CPM: {playerStats.CPM}</PlayerTypography>
     </div>
   );
