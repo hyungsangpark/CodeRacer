5a50f11af889f521de5c1d545dfa9d6d19230134
hpar461 <68981068+hpar461@users.noreply.github.com>
2022-05-20 14:17:49 +1200

    Add tests to MainContentsContainer component. (#139)
    * Rename PageContainer test
    
    * Add test case to MainContentsContainer component
    
    * Comment MainContentsContainer component.

--- frontend/src/components/MainContentsContainer/MainContentsContainer.tsx
+++ frontend/src/components/MainContentsContainer/MainContentsContainer.tsx
@@ -1,5 +1,9 @@
 import { styled } from "@mui/material";
 
+/**
+ * Div container used to wrap the main contents of the page, which is contents below header and above buttons.
+ * Using this component ensures that buttons which follow this component are placed in an identical location.
+ */
 export default styled("div")({
   display: "flex",
   flexDirection: "column",
